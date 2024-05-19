0e571ffb8878ed56910f43d5ae4b3c15bfc48da2
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-05-20 00:07:38 +0530

    Add files via upload

---
+++ recursive_factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int factorial(int number)
+{
+	if(number == 1 || number == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return (number *factorial(number -1));
+	}
+}
+
+int main()
+{
+	int num;
+	printf("The number whose factorial you want\n");
+	scanf("%d",&num);
+	printf("the factorial of %d is %d",num,factorial(num));
+	return 0;
+}
